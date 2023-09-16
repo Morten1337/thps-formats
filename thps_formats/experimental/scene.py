e56d6020d4aeb7d25d6109237a7c4524c9b4b5cb
Morten1337 <mortenkake@gmail.com>
2023-09-16 04:10:39 +0200

    generic scene format notes

---
+++ thps_formats/experimental/scene.py
@@ -0,0 +1,36 @@
+# scene>sector>split
+
+# generic format:
+# - sector contains all vertices in arrays
+# - triangles stored per split/mesh
+# - unknown
+# 	- lod levels???
+# 	- multiple vertex buffers??? for effects only?
+
+# thps3 notes:
+# - sector vertices shared for render and collision
+
+# thug1/thps4 notes:
+# - sector contains all vertices in arrays
+# - triangle strips stored per split/mesh
+
+# thug2 notes:
+# - interleaved vertices per split/mesh
+# - triangle strips stored per split/mesh
+
+# blender collision notes:
+# - does not handle double sided triangles
+# 	- same three vertices cannot be shared by multiple triangles
+# 		- ie. two triangles with different winding order...
+# 		- solution: duplicate the vertices? special flag?
+# 	- discarded on import...
+# 	- maybe detect double sided triangles when making generic scene
+# 	- ensure that there are no double sided triangles with different face flags
+
+# tests/assumptions:
+# - ensure that there are no double sided triangles with different face flags 
+# - ensure that render and collision vertices can be consolidated
+# 	- normals? colors? intensity?
+# 	- floating point precision?
+# - check the differences between multiple vertex buffer
+# 	- possibly only colors are different?
