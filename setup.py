0a7c91d504eccb3deda00bccd9f079dd55943f71
Morten1337 <mortenkake@gmail.com>
2024-08-13 21:43:24 +0200

    pre stuff – now using `pylzss` module from pypi again

--- setup.py
+++ setup.py
@@ -1,26 +1,8 @@
-import platform
-from setuptools import setup, find_packages, Extension
-
-# -------------------------------------------------------------------------------------------------
-if platform.system() == 'Windows':
-    extra_compile_args = ['/W0'] # msvc
-else:
-    extra_compile_args = ['-Wno-everything'] # clang/gcc
-
-# -------------------------------------------------------------------------------------------------
-ext_modules = [
-    Extension(
-        'thps_formats.encoding.lzss',
-        sources=['thps_formats/encoding/pylzss.c'],
-        include_dirs=['thps_formats/encoding/include'],
-        extra_compile_args=extra_compile_args,
-    )
-]
+from setuptools import setup, find_packages
 
 # -------------------------------------------------------------------------------------------------
 setup(
     name='thps_formats',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
-    ext_modules=ext_modules,
 )
