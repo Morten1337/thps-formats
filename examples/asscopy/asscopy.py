d8e70b90e54e592b8fdc44a5e9d8b2d6e53e77be
Morten1337 <mortenkake@gmail.com>
2024-09-24 00:11:40 +0200

    asscopy – this never even worked 👍

--- examples/asscopy/asscopy.py
+++ examples/asscopy/asscopy.py
@@ -5,15 +5,15 @@ from pathlib import Path
 
 
 # ------------------------------------------------------------------------------
-def parse_assets_file(input_file_path):
+def parse_assets_file(assets_file):
     assets = []
-    with input_file_path.open('r') as file:
+    with assets_file.open('r') as file:
         for line in file:
             cleaned_line = line.split(';')[0].strip()
             if not cleaned_line:
                 continue
-            file_name = Path(cleaned_line).resolve()
-            if not file_name.is_file():
+            file_name = Path(cleaned_line)
+            if not file_name.resolve().is_file():
                 raise Exception(F"Couldn't find the file with name! {file_name}")
             assets.append(file_name)
     return assets
@@ -30,11 +30,11 @@ def copy(args):
     if not args.output:
         raise Exception('No output file or directory specified!')
 
-    input_path = Path(args.input).resolve()
+    assets_file = Path(args.input).resolve()
     output_path = Path(args.output).resolve()
     source_path = Path().cwd().resolve()
 
-    assets.extend(parse_assets_file(input_path))
+    assets.extend(parse_assets_file(assets_file))
 
     for asset in assets:
         output_file = output_path / asset
